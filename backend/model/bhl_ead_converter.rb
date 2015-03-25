82d7559db1f06cd9ffb703e26cc85f11f4576501
djpillen@umich.edu <djpillen@umich.edu>
2015-03-25 09:58:24 -0400

    Comments
    Still trying to fix comments/spacing

--- backend/model/bhl_ead_converter.rb
+++ backend/model/bhl_ead_converter.rb
@@ -25,11 +25,15 @@ class BHLEADConverter < EADConverter
   def self.configure
     super
 
-# The stock EAD converter creates separate index items for each indexentry, one for the value (persname, famname, etc) and one for the reference (ref)
-# even when they are within the same indexentry and are related (i.e., the persname is a correspondent, the ref is a date or a location at which 
-# correspondence with that person can be found). The Bentley's <indexentry>s generally look something like: 
-# <indexentry><persname>Some person</persname><ref>Some date or folder</ref></indexentry>
-# As the <persname> and the <ref> are associated with one another, we want to keep them together in the same index item in ArchiveSpace. 
+# The stock EAD converter creates separate index items for each indexentry, 
+# one for the value (persname, famname, etc) and one for the reference (ref),
+# even when they are within the same indexentry and are related 
+# (i.e., the persname is a correspondent, the ref is a date or a location at which 
+# correspondence with that person can be found). 
+# The Bentley's <indexentry>s generally look something like: 
+# # <indexentry><persname>Some person</persname><ref>Some date or folder</ref></indexentry>
+# # As the <persname> and the <ref> are associated with one another, 
+# we want to keep them together in the same index item in ArchiveSpace. 
 
 # First we set the stock indexentry actions to ignore to avoid running each indexentry/x and indexentry/ref multiple times.
 	{
@@ -54,7 +58,9 @@ class BHLEADConverter < EADConverter
     end
 
 
-# This will treat each <indexentry> as one item, creating an index item with a 'value' from the <persname>, <famname>, etc. and a 'reference_text' from the <ref>. 
+# This will treat each <indexentry> as one item, 
+# creating an index item with a 'value' from the <persname>, <famname>, etc.
+# and a 'reference_text' from the <ref>. 
 
 with 'indexentry' do
 	
@@ -117,46 +123,50 @@ with 'indexentry' do
 	
 
 # The Bentley has many EADs with <dao> tags that lack title attributes. 
-# The stock ArchivesSpace EAD Converter uses each <dao>'s title attribute as the value for the imported digital object's title, which is a required property. 
+# The stock ArchivesSpace EAD Converter uses each <dao>'s title attribute as 
+# the value for the imported digital object's title, which is a required property. 
 # As a result, all of our EADs with <dao> tags fail when trying to import into ArchivesSpace. 
-# This section of the BHL EAD Converter plugin modifies the stock ArchivesSpace EAD Converter by forming a string containing the digital 
-# object's parent archival object's title and date (if both exist), or just it's title (if only the title exists), or just it's date (if only the date exists) 
+# This section of the BHL EAD Converter plugin modifies the stock ArchivesSpace EAD Converter
+# by forming a string containing the digital object's parent archival object's title and date (if both exist), 
+# or just its title (if only the title exists), or just it's date (if only the date exists) 
 # and then using that string as the imported digital object's title. 
 
 with 'dao' do	  
 
 # This forms a title string using the parent archival object's title, if it exists
-	daotitle = ''
-	ancestor(:archival_object ) do |ao|
-	  if ao.title
-	  daotitle << ao.title
-	  else
-	  daotitle = nil
-	  end
-	end
+  daotitle = ''
+  ancestor(:archival_object ) do |ao|
+    if ao.title
+    daotitle << ao.title
+    else
+    daotitle = nil
+    end
+  end
 	
-# This forms a date string using the parent archival object's date expression, or its begin date - end date, or just it's begin date, if any exist
-	daodate = ''
-	ancestor(:archival_object) do |aod|
-	  if aod.dates && aod.dates.length > 0
-	    aod.dates.each do |dl|  
-	      if dl['expression'].length > 0
-	      daodate += dl['expression']
-	      elsif (dl['begin'].length > 0 and dl['end'].length > 0) and (dl['begin'] != dl['end']) and not (dl['expression'].length > 0)
-		  daodate += "#{dl['begin']} - #{dl['end']}"
-		  elsif dl['begin'].length > 0 and (dl['begin'] = dl['end']) and not (dl['expression'].length > 0)
-	      daodate += "#{dl['begin']}"
-		  else
-		  daodate = nil
-	      end
+# This forms a date string using the parent archival object's date expression,
+# or its begin date - end date, or just it's begin date, if any exist
+  daodate = ''
+  ancestor(:archival_object) do |aod|
+    if aod.dates && aod.dates.length > 0
+	  aod.dates.each do |dl|  
+	    if dl['expression'].length > 0
+	    daodate += dl['expression']
+	    elsif (dl['begin'].length > 0 and dl['end'].length > 0) and (dl['begin'] != dl['end']) and not (dl['expression'].length > 0)
+	    daodate += "#{dl['begin']} - #{dl['end']}"
+	    elsif dl['begin'].length > 0 and (dl['begin'] = dl['end']) and not (dl['expression'].length > 0)
+	    daodate += "#{dl['begin']}"
+	    else
+	    daodate = nil
 	    end
 	  end
-	end
+    end
+  end
 	
 	title = daotitle
 	date_label = daodate if daodate.length > 0
 
-# This forms a display string using the parent archival object's title and date (if both exist), or just its title or date (if only one exists)
+# This forms a display string using the parent archival object's title and date (if both exist),
+# or just its title or date (if only one exists)
 	display_string = title || ''
 	display_string += ', ' if title && date_label
     display_string += date_label if date_label
@@ -168,8 +178,7 @@ with 'dao' do
           set ancestor(:resource, :archival_object), :instances, instance
       end
 
-# We'll use either the <dao> title attribute (if it exists) or our display_string (if the title attribute does not exist) to form the new digital object's title
-
+# We'll use either the <dao> title attribute (if it exists) or our display_string (if the title attribute does not exist)
       make :digital_object, {
         :digital_object_id => SecureRandom.uuid,
 		:title => att('title') || display_string,
@@ -182,8 +191,6 @@ with 'dao' do
          }
          set ancestor(:instance), :digital_object, obj 
       end
-
     end
-
   end
 end

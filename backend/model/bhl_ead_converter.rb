94548973415a94627b1670e94d2c8e9ee31a5a92
djpillen@umich.edu <djpillen@umich.edu>
2015-06-30 14:49:55 -0400

    modifications for descgrp/list/archdesc/*

--- backend/model/bhl_ead_converter.rb
+++ backend/model/bhl_ead_converter.rb
@@ -21,20 +21,84 @@ class BHLEADConverter < EADConverter
   def self.profile
     "Convert EAD To ArchivesSpace JSONModel records"
   end
-  
+
   def format_content(content)
   	return content if content.nil?
-    content.delete!("\n") # first we remove all linebreaks, since they're probably unintentional  
+    content.delete!("\n") # first we remove all linebreaks, since they're probably unintentional
     content.gsub("<p>","").gsub("</p>","\n\n" ).gsub("<p/>","\n\n")
   		   .gsub("<lb/>", "\n\n").gsub("<lb>","\n\n").gsub("</lb>","").gsub(/\,\s?++/,"") # also remove trailing commas
   	     .strip
   end
-  
+
 
   def self.configure
     super
-    
-# The stock EAD importer imports all extents as portion = "whole" 
+
+
+# Setting some of these to ignore because we have some physdesc, container, etc.
+# Within lists in our descgrps at the end of finding aids.
+# Without setting these to ignore, ASpace both makes the list AND makes separate
+# notes for physdesc, dimension, etc. and tries to make instances out of the
+# containers, causing import errors.
+# Note: if using this in conjunction with the Yale container management plugin,
+# be sure to include the line 'next ignore if @ignore' within the with container do
+# section of the ConverterExtraContainerValues module.
+
+=begin
+    %w{container physdesc unittitle note}.each do |node_type|
+
+          with "item/archref/#{node_type}" do
+            @ignore = true
+          end
+      end
+=end
+
+with 'archdesc' do
+    @ignore = true
+    next ignore if @ignore
+end
+
+      # example of a 1:many tag:record relation (1+ <container> => 1 instance with 1 container)
+    with 'container' do
+      next ignore if @ignore
+      @containers ||= {}
+
+      # we've found that the container has a parent att and the parent is in
+      # our queue
+      if att("parent") && @containers[att('parent')]
+        cont = @containers[att('parent')]
+
+      else
+        instance_label = att("label") ? att("label").downcase : 'mixed_materials'
+        make :instance, {
+            :instance_type => instance_label
+          } do |instance|
+            set ancestor(:resource, :archival_object), :instances, instance
+        end
+
+        inst = context_obj
+
+        make :container do |cont|
+          set inst, :container, cont
+        end
+
+        cont =  inst.container
+      end
+
+      # now we fill it in
+      (1..3).to_a.each do |i|
+        next unless cont["type_#{i}"].nil?
+        cont["type_#{i}"] = att('type')
+        cont["indicator_#{i}"] = format_content( inner_xml )
+        break
+      end
+      #store it here incase we find it has a parent
+      @containers[att("id")] = cont
+
+    end
+
+
+# The stock EAD importer imports all extents as portion = "whole"
 # We have some partial extents that we want the importer to import as portion = "part"
  with 'physdesc' do
       portion = att('altrender') || 'whole' # We want the EAD importer to know when we're importing partial extents, which we indicator via the altrender attribute
@@ -44,7 +108,7 @@ class BHLEADConverter < EADConverter
       make_note_too = false
       physdesc.children.each do |child|
         if child.respond_to?(:name) && child.name == 'extent'
-          child_content = child.content.strip 
+          child_content = child.content.strip
           if extent_number_and_type.nil? && child_content =~ /^([0-9\.]+)+\s+(.*)$/
             extent_number_and_type = {:number => $1, :extent_type => $2}
           else
@@ -71,7 +135,7 @@ class BHLEADConverter < EADConverter
       end
 
       if make_note_too
-        content =  physdesc.to_xml(:encoding => 'utf-8') 
+        content =  physdesc.to_xml(:encoding => 'utf-8')
         make :note_singlepart, {
           :type => 'physdesc',
           :persistent_id => att('id'),
@@ -82,7 +146,7 @@ class BHLEADConverter < EADConverter
       end
 
     end
-    
+
 # The stock ASpace EAD importer only makes "Conditions Governing Access" notes out of <accessrestrict> tags
 # We want to also import our <accessrestrict> tags that have a restriction end date as a "Rights Statements"
 
@@ -110,11 +174,11 @@ class BHLEADConverter < EADConverter
         } do |note|
           set ancestor(:resource, :archival_object), :notes, note
         end
-      end      
+      end
     end
 
 # Now make a Rights Statement using the content from the "Conditions Governing Access" note
-# and the restriction end date from the accessrestrict/date 
+# and the restriction end date from the accessrestrict/date
 with 'accessrestrict/date' do
     ancestor(:archival_object) do |ao|
         ao.notes.each do |n|
@@ -132,16 +196,16 @@ with 'accessrestrict/date' do
         end
     end
 end
-	
+
  with 'list' do
-      next ignore if @ignore 
-       
+      next ignore if @ignore
+
       if  ancestor(:note_multipart)
-        left_overs = insert_into_subnotes 
+        left_overs = insert_into_subnotes
       elsif ancestor(:note_index) #Set this to ignore because our <ref>s have <list>s
 	    @ignore = true
 	  else
-        left_overs = nil 
+        left_overs = nil
         make :note_multipart, {
           :type => 'odd',
           :persistent_id => att('id'),
@@ -149,9 +213,9 @@ end
           set ancestor(:resource, :archival_object), :notes, note
         end
       end
-      
-      
-      # now let's make the subnote list 
+
+
+      # now let's make the subnote list
       type = att('type')
       if type == 'deflist' || (type.nil? && inner_xml.match(/<deflist>/))
         make :note_definedlist do |note|
@@ -164,24 +228,24 @@ end
           set ancestor(:note_multipart), :subnotes, note
         end
       end
-      
-      
+
+
       # and finally put the leftovers back in the list of subnotes...
-      if ( !left_overs.nil? && left_overs["content"] && left_overs["content"].length > 0 ) 
-        set ancestor(:note_multipart), :subnotes, left_overs 
-      end 
-    
+      if ( !left_overs.nil? && left_overs["content"] && left_overs["content"].length > 0 )
+        set ancestor(:note_multipart), :subnotes, left_overs
+      end
+
     end
-	
-# The stock EAD converter creates separate index items for each indexentry, 
+
+# The stock EAD converter creates separate index items for each indexentry,
 # one for the value (persname, famname, etc) and one for the reference (ref),
-# even when they are within the same indexentry and are related 
-# (i.e., the persname is a correspondent, the ref is a date or a location at which 
-# correspondence with that person can be found). 
-# The Bentley's <indexentry>s generally look something like: 
+# even when they are within the same indexentry and are related
+# (i.e., the persname is a correspondent, the ref is a date or a location at which
+# correspondence with that person can be found).
+# The Bentley's <indexentry>s generally look something like:
 # # <indexentry><persname>Some person</persname><ref>Some date or folder</ref></indexentry>
-# # As the <persname> and the <ref> are associated with one another, 
-# we want to keep them together in the same index item in ArchiveSpace. 
+# # As the <persname> and the <ref> are associated with one another,
+# we want to keep them together in the same index item in ArchiveSpace.
 
 # First we set the stock indexentry actions to ignore to avoid running each indexentry/x and indexentry/ref multiple times.
 	{
@@ -206,16 +270,16 @@ end
     end
 
 
-# This will treat each <indexentry> as one item, 
+# This will treat each <indexentry> as one item,
 # creating an index item with a 'value' from the <persname>, <famname>, etc.
-# and a 'reference_text' from the <ref>. 
+# and a 'reference_text' from the <ref>.
 
 with 'indexentry' do
-	
+
   entry_type = ''
   entry_value = ''
   entry_reference = ''
- 
+
   indexentry = Nokogiri::XML::DocumentFragment.parse(inner_xml)
 
   indexentry.children.each do |child|
@@ -226,7 +290,7 @@ with 'indexentry' do
       entry_type << 'name'
       when 'persname'
       entry_value << child.content
-      entry_type << 'person'	
+      entry_type << 'person'
       when 'famname'
       entry_value << child.content
       entry_type << 'family'
@@ -252,13 +316,13 @@ with 'indexentry' do
       entry_value << child.content
       entry_type << 'geographic_name'
     end
-	
+
     if child.name == 'ref'
     entry_reference << child.content
     end
 
   end
-	  
+
 	make :note_index_item, {
 	  :type => entry_type,
 	  :value => entry_value,
@@ -267,19 +331,19 @@ with 'indexentry' do
 	set ancestor(:note_index), :items, item
 	end
 end
-	
-	
 
-# The Bentley has many EADs with <dao> tags that lack title attributes. 
-# The stock ArchivesSpace EAD Converter uses each <dao>'s title attribute as 
-# the value for the imported digital object's title, which is a required property. 
-# As a result, all of our EADs with <dao> tags fail when trying to import into ArchivesSpace. 
+
+
+# The Bentley has many EADs with <dao> tags that lack title attributes.
+# The stock ArchivesSpace EAD Converter uses each <dao>'s title attribute as
+# the value for the imported digital object's title, which is a required property.
+# As a result, all of our EADs with <dao> tags fail when trying to import into ArchivesSpace.
 # This section of the BHL EAD Converter plugin modifies the stock ArchivesSpace EAD Converter
-# by forming a string containing the digital object's parent archival object's title and date (if both exist), 
-# or just its title (if only the title exists), or just it's date (if only the date exists) 
-# and then using that string as the imported digital object's title. 
+# by forming a string containing the digital object's parent archival object's title and date (if both exist),
+# or just its title (if only the title exists), or just it's date (if only the date exists)
+# and then using that string as the imported digital object's title.
 
-with 'dao' do	  
+with 'dao' do
 
 # This forms a title string using the parent archival object's title, if it exists
   daotitle = ''
@@ -290,13 +354,13 @@ with 'dao' do
       daotitle = nil
     end
   end
-	
+
 # This forms a date string using the parent archival object's date expression,
 # or its begin date - end date, or just it's begin date, if any exist
   daodate = ''
   ancestor(:archival_object) do |aod|
     if aod.dates && aod.dates.length > 0
-      aod.dates.each do |dl|  
+      aod.dates.each do |dl|
         if dl['expression'].length > 0
           daodate += dl['expression']
         elsif (dl['begin'].length > 0 and dl['end'].length > 0) and (dl['begin'] != dl['end']) and not (dl['expression'].length > 0)
@@ -309,7 +373,7 @@ with 'dao' do
       end
     end
   end
-	
+
   title = daotitle
   date_label = daodate if daodate.length > 0
 
@@ -330,13 +394,13 @@ with 'dao' do
     :digital_object_id => SecureRandom.uuid,
     :title => att('title') || display_string,
     } do |obj|
-      obj.file_versions <<  {   
+      obj.file_versions <<  {
       :use_statement => att('role'),
       :file_uri => att('href'),
       :xlink_actuate_attribute => att('actuate'),
       :xlink_show_attribute => att('show')
       }
-    set ancestor(:instance), :digital_object, obj 
+    set ancestor(:instance), :digital_object, obj
     end
   end
 end
